a1e7f77920037f2164d5be9723428e6a1d126b82
hong <j520123456789@gmail.com>
2023-12-09 17:42:04 +0800

    改用linearlayout, button呈現按鈕

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,16 +27,17 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
 dependencies {
-
+    //implementation("com.google.code.gson:gson:2.10.1")
+    //implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
--- app/src/main/java/com/example/cycu_notification_system/MainActivity.kt
+++ app/src/main/java/com/example/cycu_notification_system/MainActivity.kt
@@ -1,116 +1,141 @@
 package com.example.cycu_notification_system
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.ListView
 import android.widget.Spinner
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.android.volley.Request
-import com.android.volley.RequestQueue
-import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.Volley
-import com.example.cycu_notification_system.R
-import org.json.JSONException
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var listView: ListView
-    private lateinit var spinner: Spinner
-    private lateinit var queue: RequestQueue
+
     private val BASE_URL = "https://itouch.cycu.edu.tw/home/mvc"
-    private val header: HashMap<String, String> = HashMap()
-    private val catalog: MutableMap<Int, HashMap<String, String>> = mutableMapOf()
+    private val header = Headers.Builder()
+        .add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.36")
+        .build()
+
+    private lateinit var listView: ListView
+    private var catalog: HashMap<Int, CatalogItem> = HashMap()
+    private lateinit var buttonItems: ArrayList<Button>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         listView = findViewById(R.id.listView)
-        spinner = findViewById(R.id.spinner)
-        queue = Volley.newRequestQueue(this)
+        buttonItems = arrayListOf()
 
-        header["User-Agent"] =
-            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.36"
+        // 初始化六個按鈕
+        val button1 = findViewById<Button>(R.id.button1)
+        val button2 = findViewById<Button>(R.id.button2)
+        val button3 = findViewById<Button>(R.id.button3)
+        val button4 = findViewById<Button>(R.id.button4)
+        val button5 = findViewById<Button>(R.id.button5)
+        val button6 = findViewById<Button>(R.id.button6)
 
-        // 初始化載入內容
-        fetchDataBasedOnInputId(0)
+        buttonItems.apply {
+            add(button1)
+            add(button2)
+            add(button3)
+            add(button4)
+            add(button5)
+            add(button6)
+        }
 
-        // Get title
-        val titleUrl = "$BASE_URL/ann.Model.jsp?method=title"
-        val titleRequest = JsonObjectRequest(Request.Method.POST, titleUrl, null,
-            { response ->
-                try {
-                    val annTitle = response.getJSONArray("ann_title")
-                    for (i in 0 until annTitle.length()) {
-                        val item = annTitle.getJSONObject(i)
-                        val title = item.getString("name")
-                        val id = item.getString("id")
-                        catalog[i] = hashMapOf("id" to id, "title" to title)
-                        println("$i - $title")
-                    }
+        // 設置按鈕點擊事件
+        fetchDataAndNotify()
+    }
+    private fun fetchDataAndNotify() {
+        val client = OkHttpClient()
 
-                    val spinnerOptions = ArrayList<String>()
-                    catalog.forEach { (_, value) ->
-                        spinnerOptions.add(value["title"] ?: "")
-                    }
+        val requestTitle = Request.Builder()
+            .url("$BASE_URL/ann.Model.jsp?method=title")
+            .post(RequestBody.create(MediaType.parse("application/json"), "{\"perPage\":\"50\"}"))
+            .headers(header)
+            .build()
 
-                    // Populate Spinner with options
-                    val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, spinnerOptions)
-                    spinner.adapter = adapter
-
-                    // Set up Spinner listener to handle item selection
-                    spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-                        override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
-                            fetchDataBasedOnInputId(position)
-                        }
-
-                        override fun onNothingSelected(parent: AdapterView<*>?) {
-                            // Handle situation where nothing is selected
-                        }
-                    }
-
-                } catch (e: JSONException) {
-                    e.printStackTrace()
-                }
-            },
-            { error ->
-                println("無法取得標題。$error")
+        client.newCall(requestTitle).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e("FetchData", "無法取得標題。", e)
             }
-        )
-        queue.add(titleRequest)
+
+            override fun onResponse(call: Call, response: Response) {
+                val responseData = response.body()?.string()
+                responseData?.let {
+                    val jsonResponse = JSONObject(it)
+                    val titleArray = jsonResponse.getJSONArray("ann_title")
+
+                    runOnUiThread {
+                        val buttonContainer = findViewById<LinearLayout>(R.id.buttonContainer)
+                        buttonContainer.removeAllViews()
+
+                        for (i in 0 until titleArray.length()) {
+                            val item = titleArray.getJSONObject(i)
+                            val id = item.getString("id")
+                            val name = item.getString("name")
+
+                            catalog[i] = CatalogItem(id, name)
+                            val button = Button(this@MainActivity)
+                            button.text = "$i - $name"
+                            button.setOnClickListener {
+                                fetchContent(id)
+                            }
+                            buttonContainer.addView(button)
+                        }
+                    }
+                }
+            }
+        })
     }
 
-    private fun fetchDataBasedOnInputId(inputId: Int) {
-        val contentUrl = "$BASE_URL/ann.Model.jsp?method=query"
-        val contentParams: MutableMap<String, String> = mutableMapOf()
-        val selectedItem = spinner.selectedItem as? String ?: ""
-        Toast.makeText(this, "您選擇了：$selectedItem", Toast.LENGTH_SHORT).show()
-        contentParams["sn_type"] = catalog[inputId]?.get("id") ?: ""
-        contentParams["perPage"] = "50"
 
-        val contentRequest = JsonObjectRequest(Request.Method.POST, contentUrl, null,
-            { contentResponse ->
-                try {
-                    val contentList: MutableList<String> = mutableListOf()
-                    val content = contentResponse.getJSONArray("content")
-                    for (i in 0 until content.length()) {
-                        val item = content.getJSONObject(i)
-                        val contentTitle = item.getString("TITLE")
-                        println(contentTitle)
-                        contentList.add(contentTitle)
-                    }
-                    val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, contentList)
-                    listView.adapter = adapter
-                } catch (e: JSONException) {
-                    e.printStackTrace()
-                }
-            },
-            { error ->
-                println("無法取得內容。$error")
+    private fun fetchContent(id: String) {
+        val client = OkHttpClient()
+
+        val requestBody = JSONObject()
+            .put("sn_type", id)
+            .put("perPage", "50")
+            .toString()
+
+        val requestContent = Request.Builder()
+            .url("$BASE_URL/ann.Model.jsp?method=query")
+            .post(RequestBody.create(MediaType.parse("application/json"), requestBody))
+            .headers(header)
+            .build()
+
+        client.newCall(requestContent).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e("FetchContent", "無法取得內容。", e)
             }
-        )
-        queue.add(contentRequest)
+
+            override fun onResponse(call: Call, response: Response) {
+                val responseData = response.body()?.string()
+                responseData?.let {
+                    val jsonResponse = JSONObject(it)
+                    val contentArray = jsonResponse.getJSONArray("content")
+
+                    val contentList = arrayListOf<String>()
+                    for (i in 0 until contentArray.length()) {
+                        val item = contentArray.getJSONObject(i)
+                        val title = item.getString("TITLE")
+                        contentList.add(title)
+                    }
+
+                    runOnUiThread {
+                        val contentAdapter = ArrayAdapter(this@MainActivity, android.R.layout.simple_list_item_1, contentList)
+                        listView.adapter = contentAdapter
+                    }
+                }
+            }
+        })
     }
+
+    data class CatalogItem(val id: String, val title: String)
 }
