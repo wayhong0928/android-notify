7f6a66e43a40bef61366914f60e565573f0f5562
hong <j520123456789@gmail.com>
2023-12-26 20:24:11 +0800

    基本通知功能

--- app/src/main/java/com/example/cycu_notification_system/MainActivity.kt
+++ app/src/main/java/com/example/cycu_notification_system/MainActivity.kt
@@ -2,15 +2,18 @@ package com.example.cycu_notification_system
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.FirebaseApp
 
 class MainActivity : AppCompatActivity() {
     private lateinit var getpost: Getpost
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_index)
-
+        FirebaseApp.initializeApp(this)
         getpost = Getpost(this)
         getpost.initializeViews()
+        val notify = Notify(this)
+        notify.startNotifyAfterDelay()
     }
 
 }
